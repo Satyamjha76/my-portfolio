44b4f506575a76361f20fae4917e9f1206ac60fd
Satyam Jha <128842553+Satyamjha76@users.noreply.github.com>
2025-06-29 19:29:43 +0530

    Updated project.js
    Made some changes  in order of projects

--- src/config/projects.js
+++ src/config/projects.js
@@ -57,63 +57,6 @@ import jxEmployerSignup from "../assets/project_JointerX/jxEmployerSignup.jpg";
 const projects = [
   {
     id: 1,
-    name: "Sales Sahayak",
-    description: ` The majority of small-scale companies are still storing their sales, 
-        etc information about the enquiry, and pending payments in basic Excel sheets which are not that
-         good looking or understandable by a different employee who will manage this data and also they are
-          not managed properly because of that followup to the enquiry of customers are not done timely and 
-          companies loses the orders. Here our project SalesSahayak Comes into play, it will store all enquiries 
-          from customers, orders from customers, and pending payment orders. It will also store the contact 
-          information of the customer/company. Thus on our website, an employee of that company can enter data 
-          like new enquiries, new customers, regular orders which come without enquiry, pending payment etc. 
-          However, this information will be different for every employee because every employee will be handling 
-          different clients. The information on the enquiry, orders, and payments of all the employees will be visible 
-          to the owner of the company with his/her special login account. 
-            In this way, our website will help businesses to streamline their sales department.`,
-    basedon: "Asp.net(Webforms), C#, HTML, CSS",
-    status: "Completed",
-    images: [
-      sswelcomePage,
-      ssloginPage,
-      ssHomePage,
-      ssprofilePage,
-      ssaddCustomerPage,
-      ssaddEnquiryPage,
-      ssaddPaymentPage,
-      ssaddCustomerDetailsPage,
-      ssallCustomerDetailsPage,
-      ssallEnquiryDetailsPage,
-      ssallOrderDetailsPage,
-      ssallPaymentDetailsPage,
-      ssenquiryDetailsPage,
-      ssorderDetailsPage,
-    ],
-  },
-  {
-    id: 2,
-    name: "GameVerse",
-    description: `GameVerse is a desktop-based game platform developed using Pygame and Tkinter. 
-                    It offers users a collection of fun and interactive games with a simple GUI for easy navigation. 
-                    The platform allows players to choose games, track their scores or progress, and enjoy an engaging 
-                    experience—all within a unified interface. Designed for casual gamers and programming enthusiasts, 
-                    GameVerse also serves as a learning tool to help beginners understand Python programming through game 
-                    development.`,
-    basedon: "Python, Tkinter(Python Library), Pygame(Python Library)",
-    status: "Completed",
-    images: [
-      gvHomepage,
-      gvLoginpage,
-      gvSignuppage,
-      gvLearnorDie,
-      gvTressurehunt,
-      gvMurderMystryHomePage,
-      gvMurderMystry,
-      gvtictactoe,
-      gvprofile,
-    ],
-  },
-  {
-    id: 3,
     name: "JointerX",
     description: `JointerX is a streamlined mobile application that connects job seekers and
                     employers—both managed within a single platform. Built with Android Kotlin 
@@ -155,6 +98,64 @@ const projects = [
       jxEmployerSignup,
     ],
   },
+  {
+    id: 2,
+    name: "Sales Sahayak",
+    description: ` The majority of small-scale companies are still storing their sales, 
+        etc information about the enquiry, and pending payments in basic Excel sheets which are not that
+         good looking or understandable by a different employee who will manage this data and also they are
+          not managed properly because of that followup to the enquiry of customers are not done timely and 
+          companies loses the orders. Here our project SalesSahayak Comes into play, it will store all enquiries 
+          from customers, orders from customers, and pending payment orders. It will also store the contact 
+          information of the customer/company. Thus on our website, an employee of that company can enter data 
+          like new enquiries, new customers, regular orders which come without enquiry, pending payment etc. 
+          However, this information will be different for every employee because every employee will be handling 
+          different clients. The information on the enquiry, orders, and payments of all the employees will be visible 
+          to the owner of the company with his/her special login account. 
+            In this way, our website will help businesses to streamline their sales department.`,
+    basedon: "Asp.net(Webforms), C#, HTML, CSS",
+    status: "Completed",
+    images: [
+      sswelcomePage,
+      ssloginPage,
+      ssHomePage,
+      ssprofilePage,
+      ssaddCustomerPage,
+      ssaddEnquiryPage,
+      ssaddPaymentPage,
+      ssaddCustomerDetailsPage,
+      ssallCustomerDetailsPage,
+      ssallEnquiryDetailsPage,
+      ssallOrderDetailsPage,
+      ssallPaymentDetailsPage,
+      ssenquiryDetailsPage,
+      ssorderDetailsPage,
+    ],
+  },
+  {
+    id: 3,
+    name: "GameVerse",
+    description: `GameVerse is a desktop-based game platform developed using Pygame and Tkinter. 
+                    It offers users a collection of fun and interactive games with a simple GUI for easy navigation. 
+                    The platform allows players to choose games, track their scores or progress, and enjoy an engaging 
+                    experience—all within a unified interface. Designed for casual gamers and programming enthusiasts, 
+                    GameVerse also serves as a learning tool to help beginners understand Python programming through game 
+                    development.`,
+    basedon: "Python, Tkinter(Python Library), Pygame(Python Library)",
+    status: "Completed",
+    images: [
+      gvHomepage,
+      gvLoginpage,
+      gvSignuppage,
+      gvLearnorDie,
+      gvTressurehunt,
+      gvMurderMystryHomePage,
+      gvMurderMystry,
+      gvtictactoe,
+      gvprofile,
+    ],
+  },
+  
 ];
 
 export default projects;
